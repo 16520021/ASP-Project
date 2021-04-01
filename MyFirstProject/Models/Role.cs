2616fe935cb521ab3e86459b2374e75c84eb03e1
An Tran Hoang <an.tran.hoang@preciofishbone.se>
2021-04-01 14:01:03 +0700

    Add project files.

---
+++ MyFirstProject/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Models
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string RoleName { get; set; }
+        public int UserID { get; set; }
+    }
+}
---
+++ MyFirstProject/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Models
+{
+    public class User
+    {
+        public User(List<Role> roles)
+        {
+            this.Roles = roles;
+        }
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Role> Roles { get; set; }
+    }
+}
